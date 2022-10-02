3b4b3ad83176bcd209c7b9956c76042c173d2ebd
Madeline Galbreath <mdg22a@fsu.edu>
2022-10-01 20:33:45 -0400

    Improve Figures, Add Paper

--- regression.R
+++ regression.R
@@ -2,10 +2,10 @@
 glimpse(df11)
 
 #create scatter plot mapping election date and percent of vote
-ggplot(data = df11, mapping = aes(x = election_date, y=percent_vote)) + geom_point() + geom_jitter() + theme_bw()
+ggplot(data = df11, mapping = aes(x = election_date, y=percent_vote)) + geom_point() + geom_jitter() + theme_bw() + xlab("Election Date") + ylab("Percent of Vote")
 
 #create scatter plot mapping positive decentralization mentions and percent of vote
-ggplot(data = df11, mapping = aes(x = decentralize, y=percent_vote)) + geom_point() + theme_bw()
+ggplot(data = df11, mapping = aes(x = decentralize, y=percent_vote)) + geom_point() + theme_bw() + xlab("Positive Decentralization Mentions") + ylab("Percent of Vote")
 
 #calculate r of above scatter plot
 summarize(df11, cor(decentralize, percent_vote, use = "pairwise.complete.obs"))
